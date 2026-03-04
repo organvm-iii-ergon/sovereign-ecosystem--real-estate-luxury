24d3f8f025f3a36d3c3b94bd5ad792b5178e3b17
4444jPPP <etceter4@etceter4.com>
2026-03-03 21:15:10 -0500

    ci: add missing eslint config and relax unused var rules

--- eslint.config.js
+++ eslint.config.js
@@ -5,10 +5,10 @@ import reactRefresh from 'eslint-plugin-react-refresh';
 import tseslint from 'typescript-eslint';
 
 export default tseslint.config(
-  { ignores: ['dist'] },
+  { ignores: ['dist', 'node_modules'] },
   {
     extends: [js.configs.recommended, ...tseslint.configs.recommended],
-    files: ['**/*.{ts,tsx}'],
+    files: ['**/*.{ts,tsx,js,jsx}'],
     languageOptions: {
       ecmaVersion: 2020,
       globals: globals.browser,
@@ -23,6 +23,8 @@ export default tseslint.config(
         'warn',
         { allowConstantExport: true },
       ],
+      '@typescript-eslint/no-unused-vars': 'warn',
+      'no-unused-vars': 'warn',
     },
-  },
+  }
 );
